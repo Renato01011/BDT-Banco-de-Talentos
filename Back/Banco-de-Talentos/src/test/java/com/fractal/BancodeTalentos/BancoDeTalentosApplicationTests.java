d558b4e2fb812c53c9da453bebb622d8783f26aa
Jhuliño Alexis Oroya Barreto <jaob.4.95@gmail.com>
2024-02-05 19:25:25 -0500

    feat: Set up backend project with configurations

---
+++ Back/Banco-de-Talentos/src/test/java/com/fractal/BancodeTalentos/BancoDeTalentosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fractal.BancodeTalentos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BancoDeTalentosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

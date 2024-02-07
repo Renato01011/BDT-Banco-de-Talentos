363f8f5f4cb9a4f2c558d49ca8fbe3c96b256791
Renato01011 <renato.bacigalupo@utec.edu.pe>
2024-02-07 09:21:31 -0500

    fix: Changing styles format

--- Front/Banco-de-Talentos/src/app/features/dashboard/list/list.component.ts
+++ Front/Banco-de-Talentos/src/app/features/dashboard/list/list.component.ts
@@ -4,7 +4,7 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
-  styleUrls: ['./list.component.css'],
+  styleUrls: ['./list.component.scss'],
 })
 export class ListComponent implements OnInit {
   constructor(private router: Router) {}
--- Front/Banco-de-Talentos/src/app/features/talent/new-talent/new-talent.component.ts
+++ Front/Banco-de-Talentos/src/app/features/talent/new-talent/new-talent.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-new-talent',
   templateUrl: './new-talent.component.html',
-  styleUrls: ['./new-talent.component.css']
+  styleUrls: ['./new-talent.component.scss']
 })
 export class NewTalentComponent implements OnInit {
 

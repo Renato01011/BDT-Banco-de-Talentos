0bd7a65eef4be7f78b534995b75ffc65c857e4c8
Jhuliño Alexis Oroya Barreto <jaob.4.95@gmail.com>
2024-02-07 17:31:33 -0500

    feat: Install and configure PrimeNG

--- Front/Banco-de-Talentos/src/app/shared/material/material.module.ts
+++ Front/Banco-de-Talentos/src/app/shared/material/material.module.ts
@@ -1,6 +1,18 @@
 import { NgModule } from '@angular/core';
 
+import { ButtonModule } from 'primeng/button';
+import { PanelModule } from 'primeng/panel';
+import { ToolbarModule } from 'primeng/toolbar';
+import { SplitButtonModule } from 'primeng/splitbutton';
+import { InputTextModule } from 'primeng/inputtext';
+
 @NgModule({
-  exports: [],
+  exports: [
+    ButtonModule,
+    PanelModule,
+    ToolbarModule,
+    SplitButtonModule,
+    InputTextModule,
+  ],
 })
 export class MaterialModule {}

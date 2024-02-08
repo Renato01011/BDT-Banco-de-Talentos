37ee2a0a03dce86d80f1a648ddd403ceae3a98ec
Jhuliño Alexis Oroya Barreto <jaob.4.95@gmail.com>
2024-02-08 09:53:51 -0500

    feat: Implement menubar and toolbar

--- Front/Banco-de-Talentos/src/app/features/layout/layout.component.ts
+++ Front/Banco-de-Talentos/src/app/features/layout/layout.component.ts
@@ -3,14 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-layout',
   templateUrl: './layout.component.html',
-  styles: [
-  ]
+  styleUrls: ['./layout.component.scss'],
 })
 export class LayoutComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
--- Front/Banco-de-Talentos/src/app/shared/material/material.module.ts
+++ Front/Banco-de-Talentos/src/app/shared/material/material.module.ts
@@ -1,5 +1,10 @@
 import { NgModule } from '@angular/core';
 
+import { StyleClassModule } from 'primeng/styleclass';
+import { MenuModule } from 'primeng/menu';
+import { MenubarModule } from 'primeng/menubar';
+import { AvatarModule } from 'primeng/avatar';
+import { AvatarGroupModule } from 'primeng/avatargroup';
 import { ButtonModule } from 'primeng/button';
 import { PanelModule } from 'primeng/panel';
 import { ToolbarModule } from 'primeng/toolbar';
@@ -23,6 +28,11 @@ import { DropdownModule } from 'primeng/dropdown';
     ToggleButtonModule,
     CheckboxModule,
     DropdownModule,
+    StyleClassModule,
+    MenuModule,
+    AvatarModule,
+    AvatarGroupModule,
+    MenubarModule,
   ],
 })
 export class MaterialModule {}

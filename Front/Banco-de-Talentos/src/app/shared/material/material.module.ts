6003d70aa107a1a01c92cb25f7978fa83d00a95e
Renato01011 <renato.bacigalupo@utec.edu.pe>
2024-02-08 02:00:03 -0500

    feat: Adding new talent component visuals

--- Front/Banco-de-Talentos/src/app/shared/material/material.module.ts
+++ Front/Banco-de-Talentos/src/app/shared/material/material.module.ts
@@ -5,6 +5,11 @@ import { PanelModule } from 'primeng/panel';
 import { ToolbarModule } from 'primeng/toolbar';
 import { SplitButtonModule } from 'primeng/splitbutton';
 import { InputTextModule } from 'primeng/inputtext';
+import { ScrollPanelModule } from 'primeng/scrollpanel';
+import { CardModule } from 'primeng/card';
+import { ToggleButtonModule } from 'primeng/togglebutton';
+import { CheckboxModule } from 'primeng/checkbox';
+import { DropdownModule } from 'primeng/dropdown';
 
 @NgModule({
   exports: [
@@ -13,6 +18,11 @@ import { InputTextModule } from 'primeng/inputtext';
     ToolbarModule,
     SplitButtonModule,
     InputTextModule,
+    ScrollPanelModule,
+    CardModule,
+    ToggleButtonModule,
+    CheckboxModule,
+    DropdownModule,
   ],
 })
 export class MaterialModule {}

2e14d50bb0eee849bf6969205c7009fbd0fe36c9
Jhuliño Alexis Oroya Barreto <jaob.4.95@gmail.com>
2024-02-13 13:02:32 -0500

    feat: Add a responsive control panel component

--- Front/Banco-de-Talentos/src/app/features/features.module.ts
+++ Front/Banco-de-Talentos/src/app/features/features.module.ts
@@ -4,9 +4,10 @@ import { CommonModule } from '@angular/common';
 import { FeaturesRoutingModule } from './features-routing.module';
 import { LayoutComponent } from './layout/layout.component';
 import { MaterialModule } from '../shared/material/material.module';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [LayoutComponent],
-  imports: [CommonModule, FeaturesRoutingModule, MaterialModule],
+  imports: [CommonModule, FeaturesRoutingModule, MaterialModule, SharedModule],
 })
 export class FeaturesModule {}
--- Front/Banco-de-Talentos/src/app/shared/shared.module.ts
+++ Front/Banco-de-Talentos/src/app/shared/shared.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { HeaderComponent } from './components/header/header.component';
+import { MaterialModule } from './material/material.module';
+import { ControlPanelComponent } from './components/control-panel/control-panel.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  declarations: [HeaderComponent, ControlPanelComponent],
+  imports: [CommonModule, MaterialModule, FormsModule],
+  exports: [HeaderComponent, ControlPanelComponent],
 })
-export class SharedModule { }
+export class SharedModule {}

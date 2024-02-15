d133763bc4a7a0b5abdf412d08029875cad639d8
Jhuliño Alexis Oroya Barreto <jaob.4.95@gmail.com>
2024-02-15 18:18:49 -0500

    feat: Add a carousel to display certifications

--- Front/Banco-de-Talentos/src/app/shared/components/certs-pers-crd/certs-pers-crd.component.ts
+++ Front/Banco-de-Talentos/src/app/shared/components/certs-pers-crd/certs-pers-crd.component.ts
@@ -1,18 +1,52 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-certs-pers-crd',
+  selector: 'shared-certs-pers-crd',
   templateUrl: './certs-pers-crd.component.html',
-  styleUrls: ['./certs-pers-crd.component.scss']
+  styleUrls: ['./certs-pers-crd.component.scss'],
 })
 export class CertsPersCrdComponent implements OnInit {
-
   addFileDialog: boolean = false;
 
-  constructor() { }
+  responsiveOptions: any[] = [];
+  products: any[] = [];
+
+  constructor() {}
 
   ngOnInit(): void {
+    this.responsiveOptions = [
+      {
+        breakpoint: '768px',
+        numVisible: 2,
+        numScroll: 2,
+      },
 
+      {
+        breakpoint: '560px',
+        numVisible: 1,
+        numScroll: 1,
+      },
+    ];
+    this.products = [
+      {
+        name: '1',
+      },
+      {
+        name: '2',
+      },
+      {
+        name: '3',
+      },
+      {
+        name: '4',
+      },
+      {
+        name: '5',
+      },
+      {
+        name: '6',
+      },
+    ];
   }
 
   openAddFileDialog() {
@@ -22,5 +56,4 @@ export class CertsPersCrdComponent implements OnInit {
   hideAddFileDialog() {
     this.addFileDialog = false;
   }
-
 }

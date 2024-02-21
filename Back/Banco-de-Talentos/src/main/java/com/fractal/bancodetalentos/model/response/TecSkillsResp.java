1b67c45c0d255cb5468f44903e8fe9d449b3853e
Jhuliño Alexis Oroya Barreto <jaob.4.95@gmail.com>
2024-02-21 17:48:17 -0500

    feat: Implement technical skills endpoint

---
+++ Back/Banco-de-Talentos/src/main/java/com/fractal/bancodetalentos/model/response/TecSkillsResp.java
@@ -0,0 +1,14 @@
+package com.fractal.bancodetalentos.model.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class TecSkillsResp {
+    private String name;
+}

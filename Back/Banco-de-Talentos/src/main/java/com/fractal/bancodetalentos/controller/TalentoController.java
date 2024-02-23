189fdb1493d73b18c309bdd1e1b7b645a227cb87
Renato01011 <renato.bacigalupo@utec.edu.pe>
2024-02-22 22:20:45 -0500

    fix: Normalizing endpoint URI and modifying CORS

--- Back/Banco-de-Talentos/src/main/java/com/fractal/bancodetalentos/controller/TalentoController.java
+++ Back/Banco-de-Talentos/src/main/java/com/fractal/bancodetalentos/controller/TalentoController.java
@@ -4,13 +4,10 @@ import com.fractal.bancodetalentos.model.request.NewTalentReq;
 import com.fractal.bancodetalentos.model.response.PostResp;
 import com.fractal.bancodetalentos.service.TalentoService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/Talento/v1")
+@RequestMapping("/api/v1/bdt/talent")
 @RequiredArgsConstructor
 public class TalentoController {
 

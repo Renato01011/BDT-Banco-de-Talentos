c087924de01235f993d56b5032ffdb2ff8cf9795
Jhuliño A. Oroya Barreto <jaob.4.95@gmail.com>
2024-03-16 13:21:21 -0500

    feat: Implement user picture endpoint

--- Back/Banco-de-Talentos/src/main/java/com/fractal/bancodetalentos/controller/MasterUsuarioController.java
+++ Back/Banco-de-Talentos/src/main/java/com/fractal/bancodetalentos/controller/MasterUsuarioController.java
@@ -2,6 +2,7 @@ package com.fractal.bancodetalentos.controller;
 
 import com.fractal.bancodetalentos.model.dto.TmUsuarioDTO;
 import com.fractal.bancodetalentos.model.request.UserReq;
+import com.fractal.bancodetalentos.model.response.UserProfileResp;
 import com.fractal.bancodetalentos.service.MasterUsuarioService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -17,9 +18,16 @@ public class MasterUsuarioController {
 
     private final MasterUsuarioService usuarioService;
 
-    @PostMapping()
     public ResponseEntity<TmUsuarioDTO> getUser(@RequestBody UserReq userReq) {
         TmUsuarioDTO usuarioDTO = usuarioService.findByUsername(userReq.getName());
         return new ResponseEntity<>(usuarioDTO, HttpStatus.OK);
+    }
+
+
+    @PostMapping("/picture")
+    public ResponseEntity<UserProfileResp> getUserPicture(@RequestBody UserReq userReq) {
+        TmUsuarioDTO usuarioDTO = usuarioService.findByUsername(userReq.getName());
+        UserProfileResp userProfileResp = UserProfileResp.builder().img(usuarioDTO.getImImagen()).build();
+        return new ResponseEntity<>(userProfileResp, HttpStatus.OK);
     }
 }

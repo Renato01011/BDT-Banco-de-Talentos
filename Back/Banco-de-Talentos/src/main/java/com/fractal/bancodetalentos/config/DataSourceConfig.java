111d6634e3da9ad5d0b7b2139a7894c09c1a0ce3
Jhuliño A. Oroya Barreto <jaob.4.95@gmail.com>
2024-04-26 17:26:32 -0500

    feat: Implement environment properties in DataSourceConfig

--- Back/Banco-de-Talentos/src/main/java/com/fractal/bancodetalentos/config/DataSourceConfig.java
+++ Back/Banco-de-Talentos/src/main/java/com/fractal/bancodetalentos/config/DataSourceConfig.java
@@ -1,21 +1,29 @@
 package com.fractal.bancodetalentos.config;
 
+import lombok.RequiredArgsConstructor;
 import org.apache.commons.dbcp2.BasicDataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 
 import javax.sql.DataSource;
 
 
 @Configuration
+@RequiredArgsConstructor
 public class DataSourceConfig {
+
+    private final Environment environment;
 
     @Bean
     public DataSource dataSource() {
         BasicDataSource dataSource = new BasicDataSource();
-        dataSource.setUrl("jdbc:mysql://localhost:33060/bt");
-        dataSource.setUsername("root");
-        dataSource.setPassword("alexiz");
+        String url = String.format("jdbc:mysql://localhost:%s/%s",
+                environment.getProperty("bdt.port"),
+                environment.getProperty("bdt.dbname"));
+        dataSource.setUrl(url);
+        dataSource.setUsername(environment.getProperty("bdt.user"));
+        dataSource.setPassword(environment.getProperty("bdt.password"));
         dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
         dataSource.setInitialSize(5);
         dataSource.setMaxTotal(10);
